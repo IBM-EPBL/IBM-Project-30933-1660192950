25b1ca836514ee63c1ff9db0f9d8dda225fb7ec6
Aishwariya-02 <113522757+Aishwariya-02@users.noreply.github.com>
2022-09-24 17:15:18 +0530

    Add files via upload

---
+++ Assignment 2/Kavisha U-950819106029/Kavisha.U-950819106029.py
@@ -0,0 +1,20 @@
+import random
+a=random.randint(1,100)
+b=random.randint(1,100)
+print((a))
+print((b))
+if(a>100):
+    print("DANGER!!! Temperature is high")
+elif(a==100):
+    print("Temperature alert")
+else:
+    print("Normal")
+if(b>50):
+    print("DANGER!!! Humidity is high")
+elif(b==50):
+    print("Humidity alert")
+else:
+    print("Normal humidity")
+
+
+    
